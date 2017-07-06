5122b799349bf1c3119b65de75dbd2a50cfcd387
Corentin Remaud <corentin@billjobs.fr>
2017-07-06 12:02:03 +0200

    Commit v1.0.1

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
 	name='python-random-en-us-diff',
-	version='1.0.0',
+	version='1.0.1',
 	description="Cool simple Python module to get random difference between American and English languages. Isn't life so beautiful ?",
 	packages=['random_en_us_diff'],
 	url='https://github.com/remaudcorentin-dev/python-random-en-us-diff',
